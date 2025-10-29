eecd3dacbab2765ef83ddef100d2ac8136ffdce9
Jiarong <jiarong.tang.daw@gmail.com>
2025-10-29 10:02:49 +0100

    ejercicioGetPost-finalizado

---
+++ Ejercicios/Ejercicio1/funciones.php
@@ -0,0 +1,26 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Obtenemos la acción del query string
+        $accion = $_GET['action'] ?? '';
+
+        $nombre = $_POST['nombre'];
+        $apellidos = $_POST['apellidos'];
+        $direccion = $_POST['direccion'];
+        $sexo = $_POST['sexo'];
+        $estudios = $_POST['estudios']?? 'off';
+        
+        if ($estudios == "on") {
+            $texto = "Si";
+        }else{
+            $texto = "No";
+        }
+        
+        echo "<b>Nombre: </b> $nombre <br>";
+        echo "<b>Apellidos: </b> $apellidos<br>";
+        echo "<b>Direccion: </b> $direccion<br>";
+        echo "<b>Sexo: </b> $sexo<br>";
+        echo "<b>Grado medio: </b> $texto<br>";
+        
+
+    }
+?>
