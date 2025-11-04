0c3d8a259d64c7cea7bb836612ac9babaa4e818f
Jiarong <jiarong.tang.daw@gmail.com>
2025-11-04 14:37:41 +0100

    ejStarwars

---
+++ Ejercicios/EjercicioVarios/starwars.php
@@ -0,0 +1,42 @@
+<?php
+    require_once 'datos.php';
+
+    global $sw;
+
+    $lista = $sw['results'];
+
+    $nuevo = array_filter($lista, function ($nave){
+        return (int) $nave['passengers'] > 100;
+    });
+
+    $listaFinal = array_map(function ($nave) {
+        return ["nombre"=> $nave['name'], "modelo" => $nave['model']];
+    },$nuevo);
+    echo "<h1>PASAJERSO > 100 </h1><br>";
+    print_r($listaFinal);
+
+    $nuevo2 = array_filter($lista, function ($nave){
+        return ((int) $nave['passengers'] <= 100 || $nave['passengers'] == "unknown");
+    });
+
+    $final = array_map(function ($nave) {
+        return ["nombre"=> $nave['name'], "modelo" => $nave['model']];
+    },$nuevo2);
+    echo "<h1>PASAJERSO <= 100 </h1><br>";
+    print_r($final);
+
+    $tabla = "<table border = '1'> <tbody>";
+    foreach($lista as $nave){
+        $tabla .= "<tr>
+            <td>{$nave['name']}</td>
+            <td>";
+        foreach($nave['films'] as $peli){
+            $tabla .= "$peli <br>";
+        }
+        $tabla .= "</td>
+            </tr>";
+    }
+    $tabla .= " </tbody> </table>";
+
+    echo $tabla;
+?>
